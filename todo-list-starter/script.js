bf476247df28fc4d603df4b1c7c300f0d0d46cc3
Behnaz <behnaz.shojaei88@gmail.com>
2024-11-11 14:33:31 +0800

    update the todo list

--- todo-list-starter/script.js
+++ todo-list-starter/script.js
@@ -1,15 +1,76 @@
+let todoTasks = ["Walk Chilli", "Make Dinner"];
+let todoTasksStatus = [false, true];
+// updateTodoList();
+
+// const todoList = document.getElementById("todo-list");
+
+// for (const [index, task] of todoTasks.entries()) {
+//     const newTodoTaskElement = createNewTodoItemElement(task, index);
+//     // Add the <li> element to the list
+//     todoList.appendChild(newTodoTaskElement);
+// }
+
+// const addTask = () => {
+
+// };
+
+const updateTodoList = () => {
+    const todoList = document.getElementById("todo-list");
+    todoList.innerHTML = "";
+    for (const [index, task] of todoTasks.entries()) {
+        const newTodoTaskElement = createNewTodoItemElement(task, index);
+        todoList.appendChild(newTodoTaskElement);
+    }
+};
+
+const createNewTodoItemElement = (task, index) => {
+    // Create a <p> element to store the task description
+    const newTodoTaskTextElement = document.createElement("p");
+    newTodoTaskTextElement.innerText = task;
+
+    // Apply a CSS class to the completed items
+    if (todoTasksStatus[index] == true) {
+    newTodoTaskTextElement.classList.add("complete");
+    }  
+
+    // Create a <li> element to contain the paragraph
+    const newTodoTaskElement = document.createElement("li");
+    newTodoTaskElement.appendChild(newTodoTaskTextElement);
+
+    const completeButtonElement = document.createElement("input");
+    completeButtonElement.type = "button";
+    completeButtonElement.value = "Completed";
+    completeButtonElement.onclick = function () {
+    toggleComplete(index);
+    };
+    newTodoTaskElement.appendChild(completeButtonElement);
+    return newTodoTaskElement;
+};
+
+const toggleComplete = (index) => {
+    // If it is complete, set it to incomplete. 
+    // If it is incomplete, set it to complete.
+    if (todoTasksStatus[index] == false) {
+      todoTasksStatus[index] = true;
+    } else {
+      todoTasksStatus[index] = false;
+    }
+      updateTodoList();
+    };
+
+// Initialize the list
+updateTodoList();
+
+
 const addTask = () => {
 
 };
 
-const updateTodoList = () => {
 
-};
 
-const createNewTodoItemElement = (task, index) => {
 
-};
 
-const toggleComplete = (index) => {
-    
-};
+
+
+
+
